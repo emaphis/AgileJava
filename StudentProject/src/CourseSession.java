975757a415f360bbfb01d91e951f323ef7270a8e
emaphis <emaphis85@gmail.com>
2024-01-12 14:00:55 -0500

    Add an ArrayList to CourseSession to store Students enrolled in course Update enroll method to store student in students list.

--- StudentProject/src/CourseSession.java
+++ StudentProject/src/CourseSession.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+
+
 /**
  * Class representing a Course Session
  * @author emaph
@@ -6,11 +9,14 @@
 class CourseSession {
     private String department;
     private String number;
-    private int numberOfStudents = 0;
+    private int numberOfStudents;
+    private ArrayList<Student> students;
 
     CourseSession(String department, String number) {
         this.department = department;
         this.number = number;
+        this.numberOfStudents = 0;
+        this.students = new ArrayList<Student>();
     }
 
     String getDepartment() {
@@ -25,8 +31,13 @@ class CourseSession {
         return numberOfStudents;
     }
 
-    void enroll(Student student1) {
+    void enroll(Student student) {
+        students.add(student);
         numberOfStudents++;
+    }
+
+    ArrayList<Student> getAllStudents() {
+        return students;
     }
 
 }
--- StudentProject/test/CourseSessionTest.java
+++ StudentProject/test/CourseSessionTest.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import junit.framework.TestCase;
 
 /**
@@ -20,9 +21,14 @@ public class CourseSessionTest extends TestCase {
         Student student1 = new Student("Cain DiVoe");
         session.enroll(student1);
         assertEquals(1, session.getNumberOfStudents());
+        ArrayList<Student> allStudents = session.getAllStudents();
+        assertEquals(1, allStudents.size());
 
         Student student2 = new Student("Coralee DeVaughn");
         session.enroll(student2);
         assertEquals(2, session.getNumberOfStudents());
+        assertEquals(2, allStudents.size());
+        assertEquals(student1, allStudents.get(0));
+        assertEquals(student2, allStudents.get(1));
     }
 }
