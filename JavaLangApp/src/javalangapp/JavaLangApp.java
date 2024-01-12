05159b998ad4781190bf258ec18d67d07e02405b
emaphis <emaphis85@gmail.com>
2024-01-12 02:44:18 -0500

    A scratchpad project.

---
+++ JavaLangApp/src/javalangapp/JavaLangApp.java
@@ -0,0 +1,13 @@
+package javalangapp;
+
+/**
+ * An application to experiment with Java language features.
+ * @author emaph
+ */
+public class JavaLangApp {
+
+    public static void main(String[] args) {
+        System.out.println("A Java App");
+    }
+
+}
