c756d7a3b83a182dbb837fdaff9814535fcba54a
emaphis <emaphis85@gmail.com>
2024-01-12 02:24:04 -0500

    Add a Chess Board project to do exercises.

---
+++ ChessBoard/src/chessboard/ChessApp.java
@@ -0,0 +1,16 @@
+package chessboard;
+
+/**
+ * A Chess Board Application
+ * @author emaph
+ */
+public class ChessApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("I'm a chess board!");
+    }
+
+}
