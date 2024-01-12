80f2e2832cb623d62d236a44f3555821b97c390b
emaphis <emaphis85@gmail.com>
2024-01-11 21:18:37 -0500

    Check in new StudentProject

---
+++ StudentProject/src/studentproject/StudentApp.java
@@ -0,0 +1,13 @@
+package studentproject;
+
+/**
+ * The Student Application
+ * @author emaph
+ */
+public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Students");
+    }
+
+}
---
+++ StudentProject/test/StudentTest.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+/**
+ * Tests Student class
+ * @author emaph
+ */
+public class StudentTest extends TestCase {
+
+    public void testCreate() {
+
+    }
+
+}
