5b77ed855120cfa824af757d8206a464230765c2
emaphis <emaphis85@gmail.com>
2024-02-25 07:15:52 -0500

    Add Money application from Kent Beck's TDD by example book

---
+++ Money/src/money/MoneyApp.java
@@ -0,0 +1,12 @@
+
+package money;
+
+/**
+ * Money Application class
+ * @author emaph
+ */
+public class MoneyApp {
+    public static void main(String[] args) {
+        System.out.println("Hello money!");
+    }
+}
---
+++ Money/test/money/MoneyAppTest.java
@@ -0,0 +1,16 @@
+
+package money;
+
+import junit.framework.TestCase;
+
+/**
+ * Test Application class existence
+ * @author emaph
+ */
+public class MoneyAppTest extends TestCase {
+
+    public void testApp() {
+        MoneyApp app = new MoneyApp();
+        assertTrue(app != null);
+    }
+}
