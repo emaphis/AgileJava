e48d5f1ce4aedece4091b3ca1c0aa147ab40275f
emaphis <emaphis85@gmail.com>
2024-02-25 18:29:17 -0500

    Created static factory methods for creating Francs and Dollars. Refactor: return abstract base class from times method.

--- Money/src/money/Money.java
+++ Money/src/money/Money.java
@@ -5,8 +5,18 @@ package money;
  * Common base class for money classes
  * @author emaph
  */
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
