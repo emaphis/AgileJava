9ceed0132c0579666eb0ed86b80a5e875b5f6af3
emaphis <emaphis85@gmail.com>
2024-02-25 07:29:47 -0500

    Create test and Dollar class

---
+++ Money/test/money/DollarTest.java
@@ -0,0 +1,15 @@
+package money;
+
+import junit.framework.TestCase;
+
+/**
+ * Test Dollar class
+ * @author emaph
+ */
+public class DollarTest extends TestCase {
+
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+    }
+
+}
