0cb4098bf26943f6b2b7ffbb53a12c62345e625c
emaphis <emaphis85@gmail.com>
2024-02-26 21:39:00 -0500

    Refactor: Replace Hashtable with HashMap

--- Money/src/money/Bank.java
+++ Money/src/money/Bank.java
@@ -1,22 +1,21 @@
 package money;
 
-import java.util.Hashtable;
+import java.util.HashMap;
 
 /**
  *
  * @author emaph
  */
 public class Bank {
-    private Hashtable<Pair, Integer> rates = new Hashtable<>();
-
+    private final HashMap<Pair, Integer> rates = new HashMap<>();
+    
     void addRate(String from, String to, int rate) {
-        rates.put(new Pair(from, to), new Integer(rate));
+        rates.put(new Pair(from, to), rate);
     }
 
     int rate(String from, String to) {
         if (from.equals(to)) return 1;
-        Integer rate = rates.get(new Pair(from, to));
-        return rate.intValue();
+        return rates.get(new Pair(from, to));
     }
 
     public Money reduce(Expression source, String to) {
